b784c4134dbcc74c4b59dc188de1b2e77a983182
ZcRan <360969885@qq.com>
2020-01-20 10:07:05 +0800

    gengxin

--- app/service/ex/login.ts
+++ app/service/ex/login.ts
@@ -20,19 +20,34 @@ export default class login extends Service {
 		return ResponseCode.Login_Success;
 	}
 
+	/**
+	 * 查询用户
+	 * @param uname 用户名
+	 */
 	public async hasUser(uname: string): Promise<boolean> {
 		return USER_DATA.hasOwnProperty(uname);
 	}
-
+	/**
+	 * 密码验证
+	 * @param uname 用户名
+	 * @param psw 密码
+	 */
 	public async verifPsw(uname: string, psw: string): Promise<boolean> {
 		console.log(uname, psw);
 		return USER_DATA[uname].psw === psw;
 	}
-
+	/**
+	 * 人机验证
+	 * @param value 什么什么值
+	 */
 	public async exVerif(value: string): Promise<boolean> {
 		console.log('exVerif', value);
 		return true;
 	}
+	/**
+	 * 发token
+	 * @param uname 用户名
+	 */
 	public async getToken(uname: string) {
 		console.log(uname);
 		return 'woshi token';
